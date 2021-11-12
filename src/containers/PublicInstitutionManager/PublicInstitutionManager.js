af27e7547f5a6cbcee33d09b02f3a4aedd5c809b
FAIDINE <mohamed.faidine@live.fr>
2021-11-12 16:11:24 +0100

    first commit

---
+++ src/containers/PublicInstitutionManager/PublicInstitutionManager.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import { Container } from 'react-bootstrap';
+
+class PublicInstitutionManager extends Component {
+    render() {
+        return (
+            <Container className="mt-2">
+                <h1>Projet</h1>
+            </Container>
+        )
+    };
+}
+
+export default PublicInstitutionManager;
